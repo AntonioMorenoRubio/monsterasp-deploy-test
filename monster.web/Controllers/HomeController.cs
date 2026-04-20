7e878665fcd91de4fae0f5d54cdfa356c606395e
AntonioMorenoRubio <amrinformatica10@gmail.com>
2026-04-20 16:22:51 +0200

    feat: new Pruebas page for Home Controller

--- monster.web/Controllers/HomeController.cs
+++ monster.web/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    public IActionResult Pruebas()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
