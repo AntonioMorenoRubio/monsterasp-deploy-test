4e608722f8af94eb0b76da7f811a3b4f94529914
AntonioMorenoRubio <amrinformatica10@gmail.com>
2026-04-22 16:22:50 +0200

    add: ItemDto.cs

---
+++ monster.web/Domain/DTO/ItemDto.cs
@@ -0,0 +1,8 @@
+namespace monster.web.Domain.DTO;
+
+public class ItemDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Price { get; set; }
+}
