c3e67b32ae0b00583dec34859dd02c0f44cd325e
AntonioMorenoRubio <amrinformatica10@gmail.com>
2026-04-23 10:03:47 +0200

    feat: Item class can now convert from and to ItemDto

--- monster.web/Domain/Entities/Item.cs
+++ monster.web/Domain/Entities/Item.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using monster.web.Domain.DTO;
 
 namespace monster.web.Domain.Entities;
 
@@ -8,4 +9,25 @@ public class Item
     [Key] public int Id { get; set; }
     [Required, MaxLength(50)] public string Name { get; set; } = String.Empty;
     [Required] public int Price { get; set; }
+
+    public Item() {}
+
+    public Item(ItemDto dto)
+    {
+        Id = dto.Id;
+        Name = dto.Name;
+        Price = dto.Price;
+    }
+
+    public Item FromDto(ItemDto dto) => new Item(dto);
+    
+    public ItemDto ToDto()
+    {
+        return new ItemDto()
+        {
+            Id = this.Id,
+            Name = this.Name,
+            Price = this.Price
+        };
+    }
 }
