e544e17a0bb89b2cee140161df87defa05671646
AntonioMorenoRubio <amrinformatica10@gmail.com>
2026-04-29 11:07:01 +0200

    feat: Using SqlServer and Migrating Db on startup

--- monster.web/Program.cs
+++ monster.web/Program.cs
@@ -1,9 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using monster.web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+
 var app = builder.Build();
+
+// Aplicar migraciones automáticamente (opcional)
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
