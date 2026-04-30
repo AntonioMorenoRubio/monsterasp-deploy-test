3318950a114bdcead47339aebe8eccf752b4ac63
AntonioMorenoRubio <amrinformatica10@gmail.com>
2026-04-30 11:47:12 +0200

    fix: Program.cs using SQL Server on Hosting

--- monster.web/Program.cs
+++ monster.web/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerLocal"))
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"))
 );
 
 builder.Services.AddScoped<BarcoDao>();    
